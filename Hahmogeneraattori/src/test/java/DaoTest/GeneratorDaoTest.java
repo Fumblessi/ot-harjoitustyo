f64f3d22d63ae1f7012a15a0a11c4d6c42492a4c
Sampo Suomalainen <sampo.suomalainen@helsinki.fi>
2019-12-09 22:54:35 +0200

    lisaa testeja

--- Hahmogeneraattori/src/test/java/DaoTest/GeneratorDaoTest.java
+++ Hahmogeneraattori/src/test/java/DaoTest/GeneratorDaoTest.java
@@ -13,6 +13,7 @@ import java.io.FileInputStream;
 import java.sql.*;
 import hahmogeneraattori.dao.SQLGeneratorDatabaseDao;
 import hahmogeneraattori.domain.Proficiency;
+import hahmogeneraattori.domain.Racial;
 
 /**
  *
@@ -158,6 +159,79 @@ public class GeneratorDaoTest {
         
         assertEquals(1, profFound);
         assertEquals(0, profNotFound);
+        dbDao.initialize();
+    }
+    
+    @Test
+    public void racialCanBeCreated() throws SQLException {
+        SQLGeneratorDatabaseDao dbDao = new SQLGeneratorDatabaseDao(this.testConnectionPath, 
+            this.testUser, this.testPswd);
+
+        Connection conn = dbDao.openConnection();
+        
+        Racial racial = new Racial(-1, "rasiaali", -2, false);
+        Proficiency racialProf = new Proficiency(-1, "rasiaaliprof", "Skill");
+        dbDao.create(racialProf);
+        racial.addRacialProf(racialProf);
+        
+        dbDao.create(racial);
+        int id = racial.getId();
+
+        PreparedStatement stmt = conn.prepareStatement("SELECT COUNT(*) FROM "
+                + "Racial WHERE id = ?;");
+        stmt.setInt(1, id);
+        ResultSet rs = stmt.executeQuery();
+        int racialFound = 0;
+        while (rs.next()) {
+            racialFound = rs.getInt(1);
+        }
+        stmt.close();
+        conn.close();
+        
+        assertEquals(1, racialFound);
+        dbDao.initialize();
+    }
+    
+    @Test
+    public void racialCanBeDeleted() throws SQLException {
+        SQLGeneratorDatabaseDao dbDao = new SQLGeneratorDatabaseDao(this.testConnectionPath, 
+            this.testUser, this.testPswd);
+        
+        Connection conn = dbDao.openConnection();
+        
+        Racial racial = new Racial(-1, "rasiaali", -2, false);
+        dbDao.create(racial);
+        Proficiency racialProf = new Proficiency(-1, "rasiaaliprof", "Skill");
+        dbDao.create(racialProf);
+        racial.addRacialProf(racialProf);
+        
+        int id = racial.getId();
+        
+        PreparedStatement stmt1 = conn.prepareStatement("SELECT COUNT(*) FROM "
+                + "Racial WHERE id = ?;");
+        stmt1.setInt(1, id);
+        ResultSet rs1 = stmt1.executeQuery();
+        int racialFound = 0;
+        while (rs1.next()) {
+            racialFound = rs1.getInt(1);
+        }
+        
+        dbDao.delete(racial);
+        
+        PreparedStatement stmt2 = conn.prepareStatement("SELECT COUNT(*) FROM "
+                + "Racial WHERE id = ?;");
+        stmt2.setInt(1, id);
+        ResultSet rs2 = stmt2.executeQuery();
+        int racialNotFound = 1;
+        while (rs2.next()) {
+            racialNotFound = rs2.getInt(1);
+        }
+        stmt1.close();
+        stmt2.close();
+        conn.close();
+        
+        assertEquals(1, racialFound);
+        assertEquals(0, racialNotFound);
         dbDao.initialize();
     }
 }
